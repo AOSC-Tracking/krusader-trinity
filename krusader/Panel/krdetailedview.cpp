bfcd2736bbeaf50300952984d4ab54915360294d
Darrell Anderson <humanreadable@yahoo.com>
2012-10-21 12:56:58 -0500

    Fix inadvertent tqt changes. (cherry picked from commit 61933b09acc5a4654d60e929689b73d3cf7f3bd7)

--- krusader/Panel/krdetailedview.cpp
+++ krusader/Panel/krdetailedview.cpp
@@ -1413,7 +1413,7 @@ void KrDetailedView::initProperties() {
 	TQStringList defaultAtomicExtensions;
 	defaultAtomicExtensions += ".tar.gz";
 	defaultAtomicExtensions += ".tar.bz2";
-	defaultAtomicExtensions += ".tqmoc.cpp";
+	defaultAtomicExtensions += ".moc.cpp";
 	TQStringList atomicExtensions = krConfig->readListEntry("Atomic Extensions", defaultAtomicExtensions);
 	for (TQStringList::iterator i = atomicExtensions.begin(); i != atomicExtensions.end(); )
 	{
