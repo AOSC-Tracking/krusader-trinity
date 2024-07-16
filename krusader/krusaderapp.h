8563d160a8bdc3dfa546367149b6fc8a27414198
Mingcong Bai <jeffbai@aosc.io>
2024-07-16 11:56:40 +0800

    fix(krusaderapp.h): fix compatibility with newer libX11

--- krusader/krusaderapp.h
+++ krusader/krusaderapp.h
@@ -4,6 +4,18 @@
 #include <tdeapplication.h>
 #include "X11/Xlib.h"
 
+#ifdef KeyPress
+#undef KeyPress
+#endif
+
+#ifdef GrayScale
+#undef GrayScale
+#endif
+
+#ifdef Status
+#undef Status
+#endif
+
 // declare a dummy kapplication, just to get the X focusin focusout events
 class KrusaderApp: public TDEApplication {
 	TQ_OBJECT
